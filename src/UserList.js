a33b38a7d8a4060345194fbd6ca2ff7d304a9694
Sue Sim <e.s.sim1012@gmail.com>
2020-10-07 17:20:13 +0900

    201007 commit

--- src/UserList.js
+++ src/UserList.js
@@ -1,37 +1,48 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 
 function User({ user, onRemove, onToggle }) {
-  return (
-    <div>
-      <b
+    useEffect(()=>{
+        console.log('user 값이 설정됨');
+        console.log(user);
+        return () =>{
+            console.log('컴포넌트가 화면에서 사라짐');
+            console.log(user);
+        };
+    },[user]);
+
+    return(
+
+        <div>
+        <b
         style={{
-          cursor: 'pointer',
-          color: user.active ? 'green' : 'black'
+            cursor: 'pointer',
+            color: user.active ? 'green' : 'black'
         }}
         onClick={() => onToggle(user.id)}
-      >
-        {user.username}
-      </b>
-      &nbsp;
-      <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button>
-    </div>
-  );
+        >
+            {user.username}
+        </b>
+        &nbsp;
+        <span>({user.email})</span>
+        <button onClick={()=> onRemove(user.id)}>삭제</button>
+        </div>
+    );
 }
 
-function UserList({ users, onRemove, onToggle }) {
-  return (
-    <div>
-      {users.map(user => (
-        <User
-          user={user}
-          key={user.id}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
-      ))}
-    </div>
-  );
+function UserList({users, onRemove, onToggle}){
+    return(
+        <div>
+            {users.map(user =>(
+                <User 
+                    user={user}
+                    key={user.id}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
+            ))}
+        </div>
+    );
 }
+
 
 export default UserList;
