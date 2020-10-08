d252090d480d2f08be9d6f9b38742b07b1771dac
Sue Sim <e.s.sim1012@gmail.com>
2020-10-08 16:29:03 +0900

    201008 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -9,13 +15,15 @@ function App() {
   });
 
   const { username, email } = inputs;
-  const onChange = e => {
+
+  const onChange = useCallback(
+    e => {
     const { name, value } = e.target;
-    setInputs({
+    setInputs(inputs =>({
       ...inputs,
       [name]: value
-    });
-  };
+    }));
+  }, []);
 
   const [users, setUsers] = useState([
     {
@@ -39,34 +47,42 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email
     };
-    setUsers(users.concat(user));
+    setUsers(users => users.concat(user));
 
     setInputs({
       username: '',
       email: ''
     });
+
     nextId.current += 1;
-  };
+  }, [username, email]);
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  };
+  const onRemove = useCallback(
+    id => {
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? { ...user,active : !user.active} : user
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id ? { ...user, active: !user.active } : user
         )
-    );
-  };
+      );
+    },
+    [users]
+  );
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
 
   return (
     <>
@@ -77,6 +93,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
