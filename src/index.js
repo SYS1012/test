d26ef5ade223372b72f353ffd6555738951be602
Sue Sim <e.s.sim1012@gmail.com>
2020-10-13 17:24:12 +0900

    201013 commint

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import Counter from './Counter';
-
-//ReactDOM.render(<Counter />, document.getElementById('root'));
 
 ReactDOM.render(
   <React.StrictMode>
